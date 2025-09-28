51aede3d2d366ef927502c0f31dd09d09e8b0206
noval110 <novalahmadanur@gmail.com>
2025-09-28 22:46:15 +0700

    Add files via upload

---
+++ Akhmad Noval Annur/guided 3.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+func main() {
+	var c1, c2, c3, c4, c5 byte
+	var b1, b2, b3 byte
+
+	fmt.Scan(&c1, &c2, &c3, &c4, &c5)
+
+	var str string
+	fmt.Scan(&str)
+
+	b1 = str[0]
+	b2 = str[1]
+	b3 = str[2]
+
+	fmt.Printf("%c%c%c%c%c\n", c1, c2, c3, c4, c5)
+	fmt.Printf("%c%c%c\n", b1+1, b2+1, b3+1)
+}
